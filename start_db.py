f9bbce7a0300e2d117cd6575aa3f8ff09e5111c6
Utkarsh <88526325+utkarshg20@users.noreply.github.com>
2024-07-06 03:26:57 -0400

    Delete start_db.py

--- start_db.py
+++
@@ -1,4 +0,0 @@
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import SentenceTransformersTokenTextSplitter 
-loader = TextLoader("./apug data/MATH136-notes.pdf")
-loader.load()
